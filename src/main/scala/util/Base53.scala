71196d1aa700dec688bd06e721b0f60dfc8e7df0
ShaunTS <sts_0@outlook.com>
2016-01-12 17:55:40 -0500

    Moved informative but non-specific comment from source file to readme

--- src/main/scala/util/Base53.scala
+++ src/main/scala/util/Base53.scala
@@ -1,18 +1,6 @@
 package org.sts.util
 
 
-/**
- * Base53 digits[0 - 52]:
- *  0,
- *  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
- *  A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
- *
- *
- * eg:
- *  (0, a, b, c, A, B, C) = (0, 1, 2, 3, 27, 28, 29)
- *
- *  53 (base 10) = "a0" (base 53)
- */
 object Base53 {
 
     val minLength: Int = 1
